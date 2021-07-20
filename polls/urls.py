534461a32a09ff87b00001ac3f7fc5c7a86b7dc2
alireza <easazade@gmail.com>
2021-07-20 08:04:52 +0430

    feat(blog): adding more stuff

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('form', views.form, name='form'),
+    path('add', views.add, name='add'),
+    path('subtract', views.subtract, name='subtract'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,5 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+# def index(request):
+#     return HttpResponse("poll app index page")
+
+
+# def index(request):
+#     return render(request, 'home.html')
+
+
 def index(request):
-    return HttpResponse("poll app index page")
+    return render(request, 'home.html', {'name': 'alireza'})
+
+
+def form(request):
+    return render(request, 'form.html')
+
+
+def add(request):
+    num1 = int(request.GET["num1"])
+    num2 = int(request.GET["num2"])
+    sum = num1 + num2
+    print(sum)
+    return render(request, 'result.html', {'result': sum})
+
+def subtract(request):
+    num1 = int(request.POST["num1"])
+    num2 = int(request.POST["num2"])
+    sum = num1 + num2
+    print(sum)
+    return render(request, 'result.html', {'result': sum})
