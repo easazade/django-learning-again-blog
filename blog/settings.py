f8abd8536435d5cffc921621e10ab41713063f5f
alireza <easazade@gmail.com>
2021-07-29 21:56:04 +0430

    adding model and database migration example

--- blog/settings.py
+++ blog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travel',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,8 +79,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_tut',
+        'USER': 'postgres',
+        'PASSWORD': '1321',
+        'HOST': 'localhost'
     }
 }
 
--- travel/models.py
+++ travel/models.py
@@ -3,7 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-class Destination:
-    id: int
-    name: str
-    distance: int
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='pics')
+    distance = models.IntegerField(default=0)
+    offer = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
