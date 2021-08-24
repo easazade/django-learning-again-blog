936bcd478c7817efebc933146144f468ef542095
alireza <easazade@gmail.com>
2021-08-24 21:58:30 +0430

    adding more examples

--- blog/urls.py
+++ blog/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', views.home),
     path('polls/', include('polls.urls')),
     path('travel/', include('travel.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.views import View
 from django.http import HttpResponse
+from django.shortcuts import render
 
+
+# def home(request):
+#     return HttpResponse('home page')
 
 def home(request):
-    return HttpResponse('home page')
+    return render(request, 'index.html')
