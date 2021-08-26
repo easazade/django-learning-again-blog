7aa50bb0ec5376e842cf791e9a535c2eca8a1a29
alireza <easazade@gmail.com>
2021-08-26 20:49:28 +0430

    adding example for django messages example

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+        if password1 != password2:
+            messages.info(request, 'passwords does not match')
+            print('passwords does not match')
+            return redirect('register')
+        elif User.objects.filter(username=username).exists():
+            messages.info(request, 'username already exists')
+            print('username already exists')
+            return redirect('register')
+        user = User.objects.create_user(
+            username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+        user.save()
+        print('user created')
+        return redirect('/')
+    else:
+        return render(request, 'register.html')
